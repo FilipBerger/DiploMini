ad39e7aa46fa5a63d65876e16b51855d654b3ceb
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2024-09-30 11:30:02 +0200

    Fixed property name references

--- DiploMini/GameService.cs
+++ DiploMini/GameService.cs
@@ -17,7 +17,7 @@ namespace DiploMini.Server
                 Game.Orders = orders;
             else
                 Game.Orders.AddRange(orders);
-            var player = Game.Players.Where(p => p.Id == orders[0].OwnerId).FirstOrDefault();
+            var player = Game.Players.Where(p => p.PlayerId == orders[0].OwnerId).FirstOrDefault();
             player.SubmittedOrders = true;
 
             if (Game.Players
@@ -43,14 +43,14 @@ namespace DiploMini.Server
         {
             foreach (Order order in orders)
             {
-                var targetCountry = Game.Map.FirstOrDefault(c => c.Name == order.Target);
+                var targetCountry = Game.Map.FirstOrDefault(c => c.CountryId == order.Target);
                 if (targetCountry == null) 
                     continue;
                 targetCountry.OccupyingArmy = new Army() { Id = order.ArmyId, OwnerId = order.OwnerId };
                 targetCountry.OwnerId = order.OwnerId;
                 if(order.Origin != order.Target)
                 {
-                    var originCountry = Game.Map.FirstOrDefault(c => c.Name == order.Origin);
+                    var originCountry = Game.Map.FirstOrDefault(c => c.CountryId == order.Origin);
                     originCountry.OccupyingArmy = null;
                 }
 
@@ -65,7 +65,7 @@ namespace DiploMini.Server
             {
                 Game.Players.Add(new Player()
                 {
-                    Id = i + 1,
+                    PlayerId = i + 1,
                     FactionName = playerNames[i],
                     Color = factionColors[i + 1],
                     Defeated = false
