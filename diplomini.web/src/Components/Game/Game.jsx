ffb712ac53d7b51febadf4ec945ff29b9b9c7752
Albin <Albin_hadzic@hotmail.com>
2024-10-01 15:54:09 +0200

    Implemented advanceturn in game.jsx

--- diplomini.web/src/Components/Game/Game.jsx
+++ diplomini.web/src/Components/Game/Game.jsx
@@ -3,6 +3,7 @@ import playerData from "./mockPlayers.jsx"
 import Map from "../Map/Map.jsx"
 import { getUpdatedGameState, getInitialGameState, postOrders } from "../../api.js"
 import orders from "../Map/orderFactory.jsx"
+//import PlayerTurn from "../PlayerTurn/PlayerTurn.jsx"
 
 const Game = () => {
     const [gameState , setGameState] = useState(null)
@@ -24,9 +25,26 @@ const Game = () => {
         initiateGameState()
       }, [])
 
+
+    // Goes to next player's turn or ends round
+    const turnAdvance = () => {
+        let nextPlayer = currentPlayerId + 1;
+        while (nextPlayer < gameState.players.length && gameState.players[nextPlayer].Defeated) {
+            nextPlayer++;
+        }
+        if (nextPlayer <= gameState.players.length) {
+            setCurrentPlayerId(nextPlayer);
+        } else {
+            setCurrentPlayerId(1);
+        }
+    };
     const submitOrders = async () => {
         try {
             const response = await postOrders(updatedOrders)
+            if(response.ok){
+                turnAdvance();
+                updateGameState();
+            }
         }
         catch (error) {
             console.error("Error submitting orders: ", error.message);
@@ -77,8 +95,10 @@ const Game = () => {
         
         <div>
             {gameState ? <p>Date: {gameState.ingameDate}</p> : <p>Loading...</p>}
-            {gameState ?  (<Map mapData={gameState.map} playerData={playerData} updatedOrders={updatedOrders} setUpdatedOrders={setUpdatedOrders} currentPlayerId={currentPlayerId}/>) : <p>Loading...</p>}
-            <button onClick={updateGameState} >Update Game State</button>
+            {/* {gameState ? <PlayerTurn playerData={gameState.players}/> : <p>Loading...</p>} */}
+            {gameState ? <h2>Player {gameState.players[currentPlayerId-1].factionName}'s turn</h2> : <p>Loading...</p>}
+            {gameState ?  (<Map mapData={gameState.map} playerData={gameState.players} updatedOrders={updatedOrders} setUpdatedOrders={setUpdatedOrders} currentPlayerId={currentPlayerId}/>) : <p>Loading...</p>}
+            {/* <button onClick={updateGameState} >Update Game State</button> */}
             <button onClick={submitOrders}>Submit Orders</button>
 
         </div>
