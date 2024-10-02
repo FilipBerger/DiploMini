080e4f73fdb99039baf82d1e3bf27d0ec4e67b88
Filip Berger <filip-berger@hotmail.com>
2024-10-02 20:49:21 +0200

    Formated code in SelectOrderDialog.jsx

--- diplomini.web/src/Components/SelectOrderDialog/SelectOrderDialog.jsx
+++ diplomini.web/src/Components/SelectOrderDialog/SelectOrderDialog.jsx
@@ -1,7 +1,7 @@
 import colorData from "../../../colorData";
 
 const SelectOrderDialog = (
-  {onSelectOption, players}
+  { onSelectOption, players }
 ) => {
 
   const options = [
@@ -15,45 +15,46 @@ const SelectOrderDialog = (
 
   return (
     <>
-      <div 
-      style={{
-        display: 'flex', 
-        flexDirection: 'column', 
-        maxWidth: '50%', 
-        position: 'absolute', 
-        top: '50%', 
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 1000,
-        backgroundColor: 'grey',
-        opacity: '0.9'}}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: '50%',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1000,
+          backgroundColor: 'grey',
+          opacity: '0.9'
+        }}>
         <h3>Select order</h3>
         {options.map((option) => ( // Use parentheses for clarity
-          <button 
-          key={option.Description} 
-          onClick={() => onSelectOption(option.Support)} 
-          style={{
-            backgroundColor: option.Color, 
-            opacity: '0.9',
-            borderRadius: '0'
+          <button
+            key={option.Description}
+            onClick={() => onSelectOption(option.Support)}
+            style={{
+              backgroundColor: option.Color,
+              opacity: '0.9',
+              borderRadius: '0'
             }}>
             <p>{option.Description}</p>
           </button>
         ))}
-      </div>     
-      <div 
-      onClick={() => onSelectOption(false)} 
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'black',
-        opacity: '0.3',
-        zIndex: 999
-      }}
-    /> 
+      </div>
+      <div
+        onClick={() => onSelectOption(false)}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'black',
+          opacity: '0.3',
+          zIndex: 999
+        }}
+      />
     </>
   );
 };
